c53c97dc83506aa180c0cf87553f9459a989f4a1
Cristian <aa.cristian@gmail.com>
2023-10-16 03:18:11 -0300

    - Se agregó el filtro por sucursales en frmRegistrarTurno y frmAgenda. - Se agrega funcionando la bitácora de cambios para la tabla Turnos (creación, modificación y eliminación) y los permisos correspondientes. - Se agregan los forms para el manejo de muestras.

---
+++ Application.UI/Backup/frmBackupRestore.Designer.cs
@@ -0,0 +1,135 @@
+﻿
+namespace Application.UI.Backup
+{
+    partial class frmBackupRestore
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGoBackup = new System.Windows.Forms.Button();
+            this.btnRestoreFolder = new System.Windows.Forms.Button();
+            this.btnGoRestore = new System.Windows.Forms.Button();
+            this.btnBackupFolder = new System.Windows.Forms.Button();
+            this.txtRestoreBackup = new Application.UI.Customs_Controls.TextBoxLabeled();
+            this.txtBackupDatabase = new Application.UI.Customs_Controls.TextBoxLabeled();
+            this.SuspendLayout();
+            // 
+            // btnGoBackup
+            // 
+            this.btnGoBackup.Location = new System.Drawing.Point(519, 45);
+            this.btnGoBackup.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnGoBackup.Name = "btnGoBackup";
+            this.btnGoBackup.Size = new System.Drawing.Size(124, 35);
+            this.btnGoBackup.TabIndex = 2;
+            this.btnGoBackup.Tag = "iniciar backup";
+            this.btnGoBackup.Text = "Iniciar Backup";
+            this.btnGoBackup.UseVisualStyleBackColor = true;
+            this.btnGoBackup.Click += new System.EventHandler(this.btnGoBackup_Click);
+            // 
+            // btnRestoreFolder
+            // 
+            this.btnRestoreFolder.Location = new System.Drawing.Point(470, 135);
+            this.btnRestoreFolder.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnRestoreFolder.Name = "btnRestoreFolder";
+            this.btnRestoreFolder.Size = new System.Drawing.Size(40, 35);
+            this.btnRestoreFolder.TabIndex = 3;
+            this.btnRestoreFolder.Text = "...";
+            this.btnRestoreFolder.UseVisualStyleBackColor = true;
+            this.btnRestoreFolder.Click += new System.EventHandler(this.btnRestoreFolder_Click_1);
+            // 
+            // btnGoRestore
+            // 
+            this.btnGoRestore.Location = new System.Drawing.Point(519, 134);
+            this.btnGoRestore.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnGoRestore.Name = "btnGoRestore";
+            this.btnGoRestore.Size = new System.Drawing.Size(124, 35);
+            this.btnGoRestore.TabIndex = 4;
+            this.btnGoRestore.Tag = "iniciar restore";
+            this.btnGoRestore.Text = "Iniciar Restore";
+            this.btnGoRestore.UseVisualStyleBackColor = true;
+            this.btnGoRestore.Click += new System.EventHandler(this.btnGoRestore_Click);
+            // 
+            // btnBackupFolder
+            // 
+            this.btnBackupFolder.Location = new System.Drawing.Point(470, 46);
+            this.btnBackupFolder.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnBackupFolder.Name = "btnBackupFolder";
+            this.btnBackupFolder.Size = new System.Drawing.Size(40, 35);
+            this.btnBackupFolder.TabIndex = 5;
+            this.btnBackupFolder.Text = "...";
+            this.btnBackupFolder.UseVisualStyleBackColor = true;
+            this.btnBackupFolder.Click += new System.EventHandler(this.btnBackupFolder_Click);
+            // 
+            // txtRestoreBackup
+            // 
+            this.txtRestoreBackup.LabelText = "Restaurar Backup";
+            this.txtRestoreBackup.Location = new System.Drawing.Point(18, 108);
+            this.txtRestoreBackup.Margin = new System.Windows.Forms.Padding(6, 8, 6, 8);
+            this.txtRestoreBackup.Name = "txtRestoreBackup";
+            this.txtRestoreBackup.Size = new System.Drawing.Size(442, 62);
+            this.txtRestoreBackup.TabIndex = 1;
+            this.txtRestoreBackup.Tag = "restaurar backup";
+            this.txtRestoreBackup.TextBoxText = "";
+            // 
+            // txtBackupDatabase
+            // 
+            this.txtBackupDatabase.LabelText = "Backup Database";
+            this.txtBackupDatabase.Location = new System.Drawing.Point(18, 18);
+            this.txtBackupDatabase.Margin = new System.Windows.Forms.Padding(6, 8, 6, 8);
+            this.txtBackupDatabase.Name = "txtBackupDatabase";
+            this.txtBackupDatabase.Size = new System.Drawing.Size(442, 62);
+            this.txtBackupDatabase.TabIndex = 0;
+            this.txtBackupDatabase.TextBoxText = "";
+            // 
+            // frmBackupRestore
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(669, 202);
+            this.Controls.Add(this.btnBackupFolder);
+            this.Controls.Add(this.btnGoRestore);
+            this.Controls.Add(this.btnRestoreFolder);
+            this.Controls.Add(this.btnGoBackup);
+            this.Controls.Add(this.txtRestoreBackup);
+            this.Controls.Add(this.txtBackupDatabase);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "frmBackupRestore";
+            this.Text = "frmBackup";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.frmBackupRestore_FormClosed);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Customs_Controls.TextBoxLabeled txtBackupDatabase;
+        private Customs_Controls.TextBoxLabeled txtRestoreBackup;
+        private System.Windows.Forms.Button btnGoBackup;
+        private System.Windows.Forms.Button btnRestoreFolder;
+        private System.Windows.Forms.Button btnGoRestore;
+        private System.Windows.Forms.Button btnBackupFolder;
+    }
+}
---
+++ Application.UI/Backup/frmBackupRestore.cs
@@ -0,0 +1,86 @@
+﻿using Application.ABSTRACTIONS;
+using Application.Services;
+using Application.UI.Language;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Application.UI.Backup
+{
+    public partial class frmBackupRestore : Form, ILanguageObserver
+    {
+        BLL.BackupRestore backupRestore;
+        public frmBackupRestore()
+        {
+            InitializeComponent();
+            backupRestore = new BLL.BackupRestore();
+        }
+
+
+        private void btnBackupFolder_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            dialog.ShowDialog();
+            if (dialog.SelectedPath != string.Empty)
+            {
+                txtBackupDatabase.TextBoxText = dialog.SelectedPath;               
+            }
+        }
+
+        private void btnRestoreFolder_Click_1(object sender, EventArgs e)
+        {
+            var filePath = string.Empty;
+
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = "database backup files (*.bak)|*.bak|All files (*.*)|*.*";
+                openFileDialog.FilterIndex = 1;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    //Get the path of specified file
+                    filePath = openFileDialog.FileName;
+                }
+                txtRestoreBackup.TextBoxText = filePath;
+            }
+
+        }
+
+        private void btnGoBackup_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtBackupDatabase.TextBoxText))
+                MessageBox.Show(backupRestore.BackupDatabase(txtBackupDatabase.TextBoxText));
+            else
+                MessageBox.Show("Seleccione la ruta donde guardar el backup");
+        }
+
+        private void btnGoRestore_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtRestoreBackup.TextBoxText))
+            {                
+                MessageBox.Show(backupRestore.RestoreDatabase(txtRestoreBackup.TextBoxText));
+            }
+            else
+                MessageBox.Show("Seleccione el archivo de backup que desea restaurar");
+        }
+
+        private void frmBackupRestore_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            SessionManager.GetInstance.UnsubscribeObserver(this);
+        }
+
+        public void updateLanguage(ILanguage language)
+        {
+            Translator.Translate(this);
+        }
+    }
+}
